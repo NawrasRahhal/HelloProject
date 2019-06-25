8300e5b1b8ae33fded0b48c3aae9b198f313094e
Nawras Rahhal <nawras@logitudeworld.com>
2019-06-25 13:17:36 +0300

    from child in visual studio change

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("child change");
+            Console.Write("from child in visual studio change");
         }
     }
 }
